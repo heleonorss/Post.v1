53da048ff688406d504477f7e0e689f744fceaa2
heleonorss <heleonorss@yandex.ru>
2022-05-30 01:56:59 +0300

    Add Nullable

--- src/main/kotlin/Post.kt
+++ src/main/kotlin/Post.kt
@@ -5,11 +5,11 @@ data class Post(
     val createdBy: Int, //Идентификатор администратора, который опубликовал запись
     val date: Int, //Время публикации записи в формате unixtime.
     val text: String, //Текст записи
-    val replyOwnerId: Int, //Идентификатор владельца записи, в ответ на которую была оставлена текущая.
-    val replyPostId: Int, //Идентификатор записи, в ответ на которую была оставлена текущая.
+    val replyOwnerId: Int?, //Идентификатор владельца записи, в ответ на которую была оставлена текущая.
+    val replyPostId: Int?, //Идентификатор записи, в ответ на которую была оставлена текущая.
     val friendsOnly: Boolean, // true если запись была создана с опцией «Только для друзей
     val postType: String, // Тип записи, может принимать следующие значения: post, copy, reply, postpone, suggest.
-    val signerId: Int, //Идентификатор автора, если запись была опубликована от имени сообщества и подписана пользователем
+    val signerId: Int?, //Идентификатор автора, если запись была опубликована от имени сообщества и подписана пользователем
     val canPin: Boolean, //Информация о том, может ли текущий пользователь закрепить запись
     val canDelete: Boolean, //Информация о том, может ли текущий пользователь удалить запись
     val canEdit: Boolean, //Информация о том, может ли текущий пользователь редактировать запись
@@ -18,11 +18,11 @@ data class Post(
     val isFavorite: Boolean, //если объект добавлен в закладки у текущего пользователя
     val postponedId: Int, //Идентификатор отложенной записи. Это поле возвращается тогда, когда запись стояла на таймере.
     val comments: Comments,
-    val copyright: Copyright,
+    val copyright: Copyright?,
     val likes: Likes,
     val reposts: Reposts,
     val views: Views,
-    val donut: Donut
+    val donut: Donut?
 
 
 ) {
